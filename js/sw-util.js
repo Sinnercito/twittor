1f6d5e53e21b0ec7bd21ed2e16755dfe580262d4
Tincho <fontanamartin@gmail.com>
2019-10-01 11:59:38 -0300

    primera Prueba

---
+++ js/sw-util.js
@@ -0,0 +1,18 @@
+function actualizadynamic(dynamicCache, req, res){
+
+
+if(res.ok){
+    
+    return caches.open(dynamicCache).then(cache =>{
+        
+        cache.put(req, res.clone());
+      
+        return res.clone();
+    });
+
+}else{
+    
+    return res;
+}
+
+}
